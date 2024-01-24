f7ac7b48722eb78cb0d5b9b159b1b9ed3c87ac43
nthskyradiated <andy.pandaan@outlook.com>
2024-01-24 20:18:37 +0400

    fixed footer

--- client/src/components/Footer.svelte
+++ client/src/components/Footer.svelte
@@ -9,7 +9,7 @@
         <div class="socials">
         <ul class="flex flex-row mr-4 justify-evenly text-center">
             <li><a href="https://www.facebook.com/profile.php?id=61554502162958" target="blank" class="mr-8 text-4xl"><Icon icon="bxl:facebook-circle" /></a></li>
-            <li><a href="https://www.instagram.com/thepumpfactoryiloilo/" target="blank" class="mr-8 text-4xl"><Icon icon="bxl:instagram" /></a></li>
+            <li><a href="https://www.instagram.com/thepumpfactory__/" target="blank" class="mr-8 text-4xl"><Icon icon="bxl:instagram" /></a></li>
         </ul>
     </div>
 </footer>
