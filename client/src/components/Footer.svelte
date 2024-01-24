4dd095ef015bb8d340a04d86392dda54dc411840
nthskyradiated <andy.pandaan@outlook.com>
2024-01-24 20:19:30 +0400

    fixed footer

--- client/src/components/Footer.svelte
+++ client/src/components/Footer.svelte
@@ -5,7 +5,7 @@
 <footer class="flex flex-row justify-between mx-8 mt-10 pb--1">
 
 
-        <span class="text-start">Developed by Andy &copy; 2024 All Rights Reserved</span>
+        <span class="text-start">&copy; AndyP 2024 All Rights Reserved</span>
         <div class="socials">
         <ul class="flex flex-row mr-4 justify-evenly text-center">
             <li><a href="https://www.facebook.com/profile.php?id=61554502162958" target="blank" class="mr-8 text-4xl"><Icon icon="bxl:facebook-circle" /></a></li>
